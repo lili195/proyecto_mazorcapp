a059d70b801ef355e8bd4f22be3835847f544888
lili_ana <lilianah33446@gmail.com>
2023-11-19 15:49:25 -0500

    Update CropNew.vue

--- frontend/src/components/CropNew.vue
+++ frontend/src/components/CropNew.vue
@@ -43,6 +43,9 @@
 											<v-text-field label="Identificador del cultivo" outlined dense color="#3CB371"
 												autocomplete="false" v-model="state.id_crop"
 												:rules="idCropRules"></v-text-field>
+											<p>
+												<span style="color: red; font-size: larger;">{{ state.error }}</span>
+											</p>
 											<v-text-field label="Área total del cultivo (m2)" outlined dense color="#3CB371"
 												autocomplete="false" v-model="state.area" :rules="areaRules"></v-text-field>
 											<v-select label="Cantidad de plantas por m2" outlined dense color="#3CB371"
@@ -55,8 +58,8 @@
 											<v-row>
 												<v-col>
 													<v-btn v-on:click="updateTotalPlants" color="#3CB371">Obtener
-													total de plantas</v-btn>
-	
+														total de plantas</v-btn>
+
 												</v-col>
 												<v-col>
 													<v-chip>{{ state.plants_num }}</v-chip>
@@ -99,147 +102,167 @@
 	</v-container>
 </template>
 
-<script setup>
+<script>
 import { onMounted, ref, computed, reactive } from "vue";
 import useVuelidate from "@vuelidate/core";
 import { required, minLength, helpers } from '@vuelidate/validators'
 import L from "leaflet";
 import axios from 'axios';
 import { useRouter } from 'vue-router';
-const step = ref(1);
-const router = useRouter();
-const lat = ref(0);
-const lng = ref(0);
-const map = ref();
-const mapContainer = ref();
-const token = localStorage.getItem('token');
 
-const checkCredentials = () => {
-	if (localStorage.length === 0 || !token) {
-		alert('Token de inicio de sesión no encontrado')
-		router.push('/');
-	}
-}
+export default {
+	name: 'cropNew',
+	setup() {
+		const step = ref(1);
+		const router = useRouter();
+		const lat = ref(0);
+		const lng = ref(0);
+		const map = ref();
+		const mapContainer = ref();
+		const token = localStorage.getItem('token');
 
-checkCredentials();
-
-const state = reactive({
-	id_crop: '',
-	start_date: '',
-	longitude: '',
-	latitude: '',
-	area: '',
-	plants_num: '',
-	plants_m2: 0
-})
-
-const rules = computed(() => {
-	return {
-		id_crop: {
-			required: helpers.withMessage('Porfavor ingrese un identificador', required),
-		},
-		start_date: {
-			required: helpers.withMessage('Porfavor elija una fecha de inicio de siembra', required),
-		},
-		area: {
-			required: helpers.withMessage('Por favor indique el área de su cultivo (metros cuadrados)', required),
-			minLength: minLength(1)
-		},
-		plants_num: {
-			required: helpers.withMessage('Número de plantas no válido', required),
-			minLength: minLength(1)
-		},
-		plants_m2: {
-			required: helpers.withMessage('Número de plantas por m2 no válido', required),
-			minLength: minLength(1)
-		},
-	}
-})
-
-const v$ = useVuelidate(rules, state)
-
-
-
-onMounted(() => {
-	map.value = L.map(mapContainer.value).setView([5.533333, -73.367222], 13);
-	L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-		maxZoom: 19,
-		attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>",
-	}).addTo(map.value);
-
-	// Obtener la ubicación del usuario
-	getLocation();
-});
-
-const getLocation = () => {
-	if (navigator.geolocation) {
-		navigator.geolocation.watchPosition((position) => {
-			lat.value = position.coords.latitude;
-			lng.value = position.coords.longitude;
-			map.value.setView([lat.value, lng.value], 13);
-
-			L.marker([lat.value, lng.value], { draggable: true })
-				.addTo(map.value)
-				.on("dragend", (event) => {
-					// Recuperar la longitud y latitud del marcador
-					lng.value = event.target.getLatLng().lat;
-					lat.value = event.target.getLatLng().lng;
-
-					// Mostrar la longitud y latitud en la consola
-					console.log(`Latitud: ${lat.value}, Longitud: ${lng.value}`);
-				});
-		});
-	}
-}
-
-function updateTotalPlants() {
-	state.plants_num = parseInt(state.area) * parseInt(state.plants_m2);
-	if (isNaN(state.plants_num)) {
-		state.plants_num = 0;
-	}
-}
-
-function submitCrop() {
-	if (!lat.value || !lng.value) {
-		alert('El registro requiere que su ubicación sea especificada en el mapa')
-	} else {
-		v$.value.$validate()
-		if (!v$.value.$error) {
-			const userCrop = {
-				id_crop: state.id_crop,
-				start_date: state.start_date,
-				longitude: lng.value,
-				latitude: lat.value,
-				area: state.area,
-				plants_num: state.plants_num,
-				plants_m2: state.plants_m2,
-			};
-			try {
-				const config = {
-					headers: {
-						'Authorization': token,
-						'Content-Type': 'application/json'
-					}
-				};
-				axios.post('http://localhost:3000/cropNew', userCrop, config)
-					.then(response => {
-						console.log('Cultivo registrado con éxito', response.data);
-						alert('Datos guardados con éxito')
-						router.push('/session');
-					})
-					.catch(error => {
-						console.error(error);
-					});
-			} catch (error) {
-				alert('Error inesperado')
+		const checkCredentials = () => {
+			if (localStorage.length === 0 || !token) {
+				alert('Token de inicio de sesión no encontrado')
+				router.push('/');
 			}
-
-		} else {
-			alert('Datos no correctos')
 		}
-	}
-}
+
+		checkCredentials();
+
+		const state = reactive({
+			id_crop: '',
+			start_date: '',
+			longitude: '',
+			latitude: '',
+			area: '',
+			plants_num: '',
+			plants_m2: 0,
+			error: ''
+		})
+
+		const rules = computed(() => {
+			return {
+				id_crop: {
+					required: helpers.withMessage('Por favor ingrese un identificador', required),
+				},
+				start_date: {
+					required: helpers.withMessage('Por favor elija una fecha de inicio de siembra', required),
+				},
+				area: {
+					required: helpers.withMessage('Por favor indique el área de su cultivo (metros cuadrados)', required),
+					minLength: minLength(1)
+				},
+				plants_num: {
+					required: helpers.withMessage('Número de plantas no válido', required),
+					minLength: minLength(1)
+				},
+				plants_m2: {
+					required: helpers.withMessage('Número de plantas por m2 no válido', required),
+					minLength: minLength(1)
+				},
+			}
+		})
+
+		const v$ = useVuelidate(rules, state)
+
+		onMounted(() => {
+			map.value = L.map(mapContainer.value).setView([5.533333, -73.367222], 13);
+			L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+				maxZoom: 19,
+				attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>",
+			}).addTo(map.value);
+
+			getLocation();
+		});
+
+		const getLocation = () => {
+			if (navigator.geolocation) {
+				navigator.geolocation.watchPosition((position) => {
+					lat.value = position.coords.latitude;
+					lng.value = position.coords.longitude;
+					map.value.setView([lat.value, lng.value], 13);
+
+					L.marker([lat.value, lng.value], { draggable: true })
+						.addTo(map.value)
+						.on("dragend", (event) => {
+							lng.value = event.target.getLatLng().lat;
+							lat.value = event.target.getLatLng().lng;
+
+							console.log(`Latitud: ${lat.value}, Longitud: ${lng.value}`);
+						});
+				});
+			}
+		}
+
+		const updateTotalPlants = () => {
+			state.plants_num = parseInt(state.area) * parseInt(state.plants_m2);
+			if (isNaN(state.plants_num)) {
+				state.plants_num = 0;
+			}
+		}
+
+		const submitCrop = () => {
+			if (!lat.value || !lng.value) {
+				alert('El registro requiere que su ubicación sea especificada en el mapa')
+			} else {
+				v$.value.$validate()
+				if (!v$.value.$error) {
+					const userCrop = {
+						id_crop: state.id_crop,
+						start_date: state.start_date,
+						longitude: lng.value,
+						latitude: lat.value,
+						area: state.area,
+						plants_num: state.plants_num,
+						plants_m2: state.plants_m2,
+					};
+					try {
+						const config = {
+							headers: {
+								'Authorization': token,
+								'Content-Type': 'application/json'
+							}
+						};
+						axios.post('http://localhost:3000/cropNew', userCrop, config)
+							.then(response => {
+								console.log('Cultivo registrado con éxito', response.data);
+								alert('Datos guardados con éxito')
+								router.push('/session');
+							})
+							.catch(error => {
+								console.error(error);
+								state.error = error.response.data.error
+							});
+					} catch (error) {
+						alert('Error inesperado')
+					}
+				} else {
+					alert('Datos no correctos')
+				}
+			}
+		}
+
+		return {
+			step,
+			router,
+			lat,
+			lng,
+			map,
+			mapContainer,
+			token,
+			checkCredentials,
+			state,
+			rules,
+			v$,
+			getLocation,
+			updateTotalPlants,
+			submitCrop
+		};
+	},
+};
 </script>
+
 
 <style scoped>
 .crop-register label {
